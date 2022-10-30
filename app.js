c499ce5839f4e34a92902bec22a087fc8eb02b68
ViktorLumiste <viktor.lumiste@gmail.com>
2022-10-30 20:41:47 +0200

    Display weather data

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const weather = new Weather("Tartu")
+const ui = new UI()
 
 function getWeather(){
     weather.getWeather()
     .then(data => {
         console.log(data)
+        ui.drawWeather(data)
     })
         .catch(error => console.log(error))
 }
