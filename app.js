98df17bf94630332984585e42c37ebe95b953308
ViktorLumiste <viktor.lumiste@gmail.com>
2022-10-30 21:08:21 +0200

    Added option to change city with a input field

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const weather = new Weather("Tartu")
 const ui = new UI()
 
+const form = document.querySelector("#change-city")
+form.addEventListener("submit", changeWeather)
+
+function changeWeather(e){
+    const city = document.querySelector("#city-name").value
+    weather.changeCity(city)
+    getWeather()
+    document.querySelector(("#city-name")).value = ""
+    e.preventDefault()
+}
+
 function getWeather(){
     weather.getWeather()
     .then(data => {
@@ -9,5 +20,6 @@ function getWeather(){
     })
         .catch(error => console.log(error))
 }
+
 
 getWeather()
